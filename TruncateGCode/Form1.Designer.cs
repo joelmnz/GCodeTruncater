4c536fb39b8d123e39b2c4dedbdf8bc711db1003
Phillip Castellanos <z3hp@yahoo.com>
2014-09-20 21:06:40 -0600

    GCode Truncater

---
+++ TruncateGCode/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace TruncateGCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tbSource = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.tbTruncate = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnSaveFile = new System.Windows.Forms.Button();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.saveFile = new System.Windows.Forms.SaveFileDialog();
+            this.lblSts = new System.Windows.Forms.Label();
+            this.btnTruncate = new System.Windows.Forms.Button();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tbSource);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tbTruncate);
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox2);
+            this.splitContainer1.Size = new System.Drawing.Size(1126, 595);
+            this.splitContainer1.SplitterDistance = 567;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // tbSource
+            // 
+            this.tbSource.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbSource.Location = new System.Drawing.Point(0, 45);
+            this.tbSource.Multiline = true;
+            this.tbSource.Name = "tbSource";
+            this.tbSource.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbSource.Size = new System.Drawing.Size(567, 550);
+            this.tbSource.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnTruncate);
+            this.groupBox1.Controls.Add(this.btnOpenFile);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(567, 45);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(6, 12);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFile.TabIndex = 1;
+            this.btnOpenFile.Text = "&Open File";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // tbTruncate
+            // 
+            this.tbTruncate.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbTruncate.Location = new System.Drawing.Point(0, 45);
+            this.tbTruncate.Multiline = true;
+            this.tbTruncate.Name = "tbTruncate";
+            this.tbTruncate.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbTruncate.Size = new System.Drawing.Size(555, 550);
+            this.tbTruncate.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblSts);
+            this.groupBox2.Controls.Add(this.btnSaveFile);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(555, 45);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // btnSaveFile
+            // 
+            this.btnSaveFile.Location = new System.Drawing.Point(7, 11);
+            this.btnSaveFile.Name = "btnSaveFile";
+            this.btnSaveFile.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveFile.TabIndex = 1;
+            this.btnSaveFile.Text = "&Save File";
+            this.btnSaveFile.UseVisualStyleBackColor = true;
+            this.btnSaveFile.Click += new System.EventHandler(this.btnSaveFile_Click);
+            // 
+            // openFile
+            // 
+            this.openFile.Filter = "All Files|*.*";
+            this.openFile.InitialDirectory = "c:\\";
+            this.openFile.Title = "Select File";
+            // 
+            // saveFile
+            // 
+            this.saveFile.AddExtension = false;
+            this.saveFile.InitialDirectory = "c:\\";
+            this.saveFile.Title = "Save File";
+            // 
+            // lblSts
+            // 
+            this.lblSts.AutoSize = true;
+            this.lblSts.Location = new System.Drawing.Point(88, 21);
+            this.lblSts.Name = "lblSts";
+            this.lblSts.Size = new System.Drawing.Size(0, 13);
+            this.lblSts.TabIndex = 2;
+            // 
+            // btnTruncate
+            // 
+            this.btnTruncate.Location = new System.Drawing.Point(445, 12);
+            this.btnTruncate.Name = "btnTruncate";
+            this.btnTruncate.Size = new System.Drawing.Size(116, 23);
+            this.btnTruncate.TabIndex = 2;
+            this.btnTruncate.Text = "&Truncate GCode";
+            this.btnTruncate.UseVisualStyleBackColor = true;
+            this.btnTruncate.Click += new System.EventHandler(this.btnTruncate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1126, 595);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Truncate GCode";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox tbSource;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.TextBox tbTruncate;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnSaveFile;
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.SaveFileDialog saveFile;
+        private System.Windows.Forms.Label lblSts;
+        private System.Windows.Forms.Button btnTruncate;
+    }
+}
+
---
+++ TruncateGCode/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace TruncateGCode
+{
+    public partial class Form1 : Form
+    {
+        private bool end = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string truncateGCode(string code)
+        {
+            try
+            {
+
+                if ((code.Substring(0, 1) == "(") || (code.Substring(0, 1) == ";") || (code.Substring(0, 1) == ":") ||
+                    (code.Substring(0, 1) == "'"))
+                {
+                    return code;
+                }
+
+                string[] codes = code.Split(' ');
+                string finCode = "";
+
+                foreach (string gCode in codes)
+                {
+                    if (gCode.IndexOf(".") > -1)
+                    {
+                        double val = 0;
+                        if (double.TryParse(gCode.Substring(0, 1), out val))
+                        {
+                        }
+                        else
+                        {
+                            if (Convert.ToDouble(gCode.Substring(1, gCode.Length - 1)) != 0)
+                            {
+                                string num = Convert.ToDouble(gCode.Substring(1, gCode.Length - 1)).ToString("#.###");
+                                if (num == "")
+                                {
+                                    num = "0";
+                                }
+                                finCode += gCode.Substring(0, 1) + num;
+
+                            }
+                            else
+                            {
+                                finCode += gCode.Substring(0, 1) + "0";
+                            }
+                        }
+                    }
+                    else
+                    {
+                        finCode += gCode;
+                    }
+                }
+
+                return finCode;
+            }
+            catch (Exception ex)
+            {
+               
+                    MessageBox.Show(ex.Message, "Unable to Process GCode",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                 
+                end = true;
+                return "(ERROR)";
+            }
+
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            if (openFile.ShowDialog() != DialogResult.Cancel)
+            {
+                tbSource.Text = "";
+                tbTruncate.Text = "";
+                tbSource.Text = File.ReadAllText(openFile.FileName);
+                ProcessCode();
+                lblSts.Text = "";
+            }
+        }
+
+
+
+        void ProcessCode()
+        {
+            string[] lines = tbSource.Text.Split('\n');
+            foreach (string line in lines)
+            {
+                line.Replace("\r","");
+                tbTruncate.Text += truncateGCode(line) + Environment.NewLine;
+                if (end)
+                {
+                    end = false;
+                    break;
+                }
+            }
+        }
+
+        private void btnSaveFile_Click(object sender, EventArgs e)
+        {
+            if (saveFile.ShowDialog() != DialogResult.Cancel)
+            {
+                File.WriteAllText(saveFile.FileName, tbTruncate.Text);
+                lblSts.Text = "File Saved: " + saveFile.FileName;
+            }
+        }
+
+        private void btnTruncate_Click(object sender, EventArgs e)
+        {
+            tbTruncate.Text = "";
+            lblSts.Text = "";
+            ProcessCode();
+        }
+    }
+}
